557b237a4f0ad881a17a03bd1ace5fc946935510
Sándor Vass <vasssandor@hotmail.com>
2017-11-13 11:37:15 +0100

    feat(application.properties) : postgresql add

--- src/main/java/com/greenfox/rdg5/p2pchat/P2pchatApplication.java
+++ src/main/java/com/greenfox/rdg5/p2pchat/P2pchatApplication.java
@@ -1,12 +1,14 @@
 package com.greenfox.rdg5.p2pchat;
 
+import com.greenfox.rdg5.p2pchat.model.User;
 import com.greenfox.rdg5.p2pchat.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P2pchatApplication {
+public class P2pchatApplication implements CommandLineRunner {
   @Autowired
   UserRepo repo;
 
@@ -15,5 +17,10 @@ public class P2pchatApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(P2pchatApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    repo.save(new User("pisti"));
   }
 }
